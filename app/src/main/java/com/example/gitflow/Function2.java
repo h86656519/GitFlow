bba101a36d3d88918d8523ab7c872e8b26183704
h86656519 <h86656519@gmail.com>
2020-01-19 20:14:57 +0800

    [新增] 完成feature2 功能

--- app/src/main/java/com/example/gitflow/Function2.java
+++ app/src/main/java/com/example/gitflow/Function2.java
@@ -2,4 +2,11 @@ package com.example.gitflow;
 
 public class Function2 {
     //新增feture2 的功能
+
+    //練習Stash:
+    // 1.正在修改feature 2 的功能中，未完成，臨時需要切回feature1 做修改
+    // 2.修正完feature1 的bug後，繼續開發feature2
+    // 3.完成feature2 的功能了
+
+
 }
